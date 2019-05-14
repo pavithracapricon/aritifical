cfff832d7f0104b00b01714dc50ec9c595cc920f
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-05-14 14:55:20 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
 num = float(input("Enter a number: "))
-if num > 0: print("Positive number")
+if num > 0: print("Positive")
 elif num == 0: print("Zero")
-else: print("Negative number")
+else: print("Negative")
