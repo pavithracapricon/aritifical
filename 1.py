7c80b79e01ad9ef8243c948f01021951c3848151
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-05-15 09:46:41 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
-num = 8
+num = int(input("enter the number"))
 if num > 0: print("Positive")
 elif num == 0: print("Zero")
-else: print("Negative")
+elif num<0: print("Negative")
+else: print("invalid input")
