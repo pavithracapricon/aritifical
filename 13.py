1855c72e14fbec60ba41e5ec24fd57a678e3f260
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-06-17 11:06:58 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,8 +1,8 @@
 n=int(input("enter number"))
 if n>1:
  if(n%2)==0:
-  print(n,"is prime number")
+  print(yes)
  else:
-  print(n," is not a prime number")
+  print(no)
 else:
- print(n,"is not a prime number")
+ print(no)
