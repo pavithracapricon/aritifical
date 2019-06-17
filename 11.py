a1b6478beb4d210f8e926873af3d74591ad5781c
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-06-17 10:44:07 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -2,11 +2,9 @@ def power(x, y):
   
   if (y == 0): return 1
   elif (int(y % 2) == 0): 
-    return (power(x, int(y / 2)) *
-      power(x, int(y / 2))) 
+    return (power(x, int(y / 2)) * power(x, int(y / 2))) 
   else: 
-    return (x * power(x, int(y / 2)) *
-       power(x, int(y / 2))) 
+    return (x * power(x, int(y / 2)) * power(x, int(y / 2))) 
   
 x = 2; y = 3
 print(power(x, y)) 
