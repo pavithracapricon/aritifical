e9d012c2f7c79f169925dca28fdf2888c432eec0
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-06-17 18:05:46 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,7 @@
+start = int(input("Enter the start of range: ")) 
+end = int(input("Enter the end of range: "))
+for num in range(start, end + 1):
+ if (n % i) == 0:
+  print(yes)
+ else:
+  print(no)
