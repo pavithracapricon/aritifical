9661cf56783496057bf5298ece514c6083b5fbec
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-06-17 10:40:19 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,4 +1,12 @@
-
-a,b= map(int,input().split())
-c= pow(a,b)
-print(c)
+def power(x, y): 
+  
+  if (y == 0): return 1
+  elif (int(y % 2) == 0): 
+    return (power(x, int(y / 2)) *
+      power(x, int(y / 2))) 
+  else: 
+    return (x * power(x, int(y / 2)) *
+       power(x, int(y / 2))) 
+  
+x = 2; y = 3
+print(power(x, y)) 
