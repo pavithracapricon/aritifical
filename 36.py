a681daebbda5d8bb1aea573e2f2579abac33d151
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-06-29 23:54:30 +0530

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,8 @@
+
+string = input("Please Enter your Own String : ")
+alphabets = digits = special = 0
+
+for i in range(len(string)):
+  special = special + 1
+    
+print("Total Number of Special Characters in this String :  ", special)
