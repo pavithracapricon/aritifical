aea545fcf40c4bed7f6e4700550f4e23fae4797a
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-06-30 20:22:31 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+print("enter the character")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U')
+print("character" ,ch,"is an vowel")
+else:
+print("character" ,ch,"is an consonat")
