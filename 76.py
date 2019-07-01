ef0d8a6dfbc08c41f73b0cd5694b01aa6c803225
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-01 22:14:13 +0530

    Update 76.py

--- 76.py
+++ 76.py
@@ -3,21 +3,14 @@ def isComposite(n):
         return False
     if (n <= 3): 
         return False
-   
-    if (n % 2 == 0 or n % 3 == 0): 
+    if (n % 2 == 0 ): 
         return True
     i = 5
     while(i * i <= n): 
           
-        if (n % i == 0 or n % (i + 2) == 0): 
+        if (n % i == 0 ): 
             return True
         i = i + 6
           
     return False 
-print("true") if(isComposite(11))
- else 
-print("false") 
-print("true") 
-if(isComposite(15)) 
-else 
-print("false") 
+ 
