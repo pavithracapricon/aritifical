68fbc87a02f53751ef19a2c0ff9bfd53333852c7
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-08 21:38:49 +0530

    Update 6p.py

--- 6p.py
+++ 6p.py
@@ -1,11 +1,10 @@
-m,m2=map(str,input().split())
-if(len(m)!=len(m2)):
+R,R2=map(str,input().split())
+if(len(R)!=len(R2)):
    print("no")
 else:
-   k1=[m.count(i) for i in m1]
-   k2=[m2.count(i) for i in m2]
-if(k1==k2):
+   S1=[R.count(a) for a in R]
+   T1=[R2.count(a) for a in R2]
+if(S1==T1):
    print("yes")
 else:
    print("no")
-      
