0120016c269d64343b54a098c8c92168f201c765
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-08 18:57:21 +0530

    Create 7p.py

---
+++ 7p.py
@@ -0,0 +1,15 @@
+input_string=input("enter a string:")
+output_string=""
+oddorEven=int(input("enter '1' if you want to remove odd positioned characters,'2' for even positioned characters:"))
+if oddoreven==1:
+print("string after removing characters on odd position:")
+for i in range(len(input_string)):
+if i%2!=0:
+output_string=output_string+input_string[i]
+
+elif oddorEven==2:
+ print("string after removing characters on even position:")
+for i in range(len(input_string));
+if i%2==0:
+ output_string=output_string+input_string[i];
+print(output_string)
