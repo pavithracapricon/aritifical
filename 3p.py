fe4bdd876e60c806d41c9caf0e42dde91fec1801
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-08 21:12:27 +0530

    Update 3p.py

--- 3p.py
+++ 3p.py
@@ -1,7 +1,3 @@
-N= int(input("Please Enter any Number: "))    
-Reverse = 0    
-while(N> 0):    
-  Reminder = N %10    
-  Reverse = (Reverse *10) + Reminder    
-  N = N //10    
-print("\n Reverse of entered number is = %d" %Reverse) 
+    
+z=int(input()[::-1])
+print(z)
