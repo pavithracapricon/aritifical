0d77ee651dc31b3138c7977fe70fce0119d372d5
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-08 19:01:33 +0530

    Create 8p.py

---
+++ 8p.py
@@ -0,0 +1,12 @@
+def convert(s): 
+    if(len(s) == 0): 
+        return
+    s1 = '' 
+    s1 += s[0].upper() 
+    for i in range(1, len(s) - 1): 
+        if (s[i] == ' '): 
+            s1 += s[i + 1].upper() 
+            i += 1
+        elif(s[i - 1] != ' '): 
+            s1 += s[i]  
+    print(s1)      
