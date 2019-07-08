fe73ae3711684635c792bc5f0c34464f9e25ab4a
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-08 21:35:06 +0530

    Update 6p.py

--- 6p.py
+++ 6p.py
@@ -1,19 +1,11 @@
-def areIsomorphic(string1, string2): 
-    m = len(string1) 
-    n = len(string2) 
-    if m != n: 
-        return False
-    marked = [False] * MAX_CHARS  
-    map = [-1] * MAX_CHARS  
-    for i in xrange(n):
-        if map[ord(string1[i])] == -1: 
-            if marked[ord(string2[i])] == True: 
-                return False 
-            marked[ord(string2[i])] = True 
-            map[ord(string1[i])] = string2[i] 
-  
-        
- elif map[ord(string1[i])] != string2[i]: 
-            return False
-  
-    return True
+m,m2=map(str,input().split())
+if(len(m)!=len(m2)):
+   print("no")
+else:
+   k1=[m.count(i) for i in m1]
+   k2=[m2.count(i) for i in m2]
+if(k1==k2):
+   print("yes")
+else:
+   print("no")
+      
