8cf1ae0d13684a36725b776914c73e125648f2b3
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-08 21:22:39 +0530

    Update 5p.py

--- 5p.py
+++ 5p.py
@@ -1,12 +1,3 @@
- def roman_to_int(self, s):
-  rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-  int_val = 0
-  for i in range(len(s)):
-   if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-     int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
-    else:
-     int_val += rom_val[s[i]]
-        return int_val
-print(py_solution().roman_to_int('MMMCMLXXXVI'))
-print(py_solution().roman_to_int('MMMM'))
-print(py_solution().roman_to_int('C'))
+d={'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9,'X':10,'XI':11,'XII':12,'XIII':13,'XIV':14,'XV':15,'XVI':16,'XVII':17,'XVIII':18,'XIX':19,'XX':20}
+ds=input()
+print(d[ds])
