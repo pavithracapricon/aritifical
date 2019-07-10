2a4b6100b0270689d08db7f80e31c4e2962b88f5
pavithracapricon <46060937+pavithracapricon@users.noreply.github.com>
2019-07-10 18:33:00 +0530

    Update 29.py

--- 29.py
+++ 29.py
@@ -1,10 +1,10 @@
 
-time = float(input("Input time in seconds: "))
-day = time // (24 * 3600)
-time = time % (24 * 3600)
-hour = time // 3600
-time %= 3600
-minutes = time // 60
-time %= 60
-seconds = time
-print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
+t= float(input("Input time in seconds: "))
+d = t // (24 * 3600)
+t = t % (24 * 3600)
+h = t // 3600
+t%= 3600
+m = t// 60
+t %= 60
+s = t
+print("d:h:m:s-> %d:%d:%d:%d" % (d, h, m, s))
